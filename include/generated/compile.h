dd4f3de3294c91fee9f18fdf5a4dcd07c2be7959
starsoc-alex <starstar_gfgf@163.com>
2015-03-13 17:45:48 +0800

    modify device tree

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 4 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 PREEMPT Fri Mar 13 17:21:09 CST 2015"
+#define UTS_VERSION "#4 PREEMPT Fri Mar 13 17:32:14 CST 2015"
 #define LINUX_COMPILE_BY "alex"
 #define LINUX_COMPILE_HOST "alex-Vostro-470"
 #define LINUX_COMPILER "gcc version 4.6.1 (Sourcery CodeBench Lite 2011.09-50) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.6.0-digilent-13.01-g0a75960-dirty"
+#define UTS_RELEASE "3.6.0-digilent-13.01-gad91adc-dirty"
