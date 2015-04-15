ab6871f9dcb609ad3843cc3257c09a4da08ee6d5
starsoc-alex <starstar_gfgf@163.com>
2015-04-15 17:00:06 +0800

    delete all file of branch 3.18

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 24 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#24 SMP PREEMPT Fri Mar 27 20:04:20 CST 2015"
-#define LINUX_COMPILE_BY "alex"
-#define LINUX_COMPILE_HOST "alex-VirtualBox"
-#define LINUX_COMPILER "gcc version 4.6.1 (Sourcery CodeBench Lite 2011.09-50) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.17.0-xilinx-g7fe5083-dirty"
